146267815fffca9654812e8958ac0ed0bb7d6fbd
jollypyun <lucas.stave@gmail.com>
2023-01-10 16:41:16 +0900

    테스트 추가 및 수정

--- src/main/java/calculator/NewCalculator.java
+++ src/main/java/calculator/NewCalculator.java
@@ -1,29 +1,40 @@
 package calculator;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class NewCalculator {
-    private static final String REGEXP = "^[0-9]*$";
     private int result = 0;
-    private Operator operator;
+    private Operator operator = Operator.PLUS;;
 
     public int execute() {
         Scanner scanner = new Scanner(System.in);
-        String value = scanner.nextLine();
-        String[] values = value.split(" ");
-
-        for(String input : values) {
-            calculateOrChangeOperate(input);
+        String value;
+        String[] values;
+        try {
+            value = scanner.nextLine();
+            values = value.split(" ");
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("공백 입력은 불가하다.");
         }
-        return result;
+        scanner.close();
+
+        try {
+            return calculate(values);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ArrayIndexOutOfBoundsException("입력이 잘못 되었다.");
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("숫자 자리에 다른 것을 입력했거나 띄어쓰기가 잘못 되었다.");
+        }
     }
 
-    private void calculateOrChangeOperate(String input) {
-        if (Pattern.matches(REGEXP, input)) {
-            result = operator.operate(result, Integer.parseInt(input));
-            return ;
+    private int calculate(String[] input) {
+        result = Integer.parseInt(input[0]);
+        for(int i = 1; i < input.length; i += 2) {
+            operator = Operator.findOperate(input[i]);
+            result = operator.operate(result, Integer.parseInt(input[i+1]));
         }
-        operator = Operator.findOperate(input);
+        return result;
     }
 }
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,12 +1,15 @@
 package study;
 
 import calculator.NewCalculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
     NewCalculator calculator = new NewCalculator();
@@ -16,34 +19,109 @@ public class CalculatorTest {
     }
 
     @Test
-    void mixCal() {
+    @DisplayName("복합 계산")
+    void mixCalTest() {
         inputString("2 + 3 - 0 * 4 / 2");
-        calculator.execute();
         assertThat(calculator.execute()).isEqualTo(10);
     }
 
     @Test
-    void plus() {
-
+    @DisplayName("더하기 테스트")
+    void plusTest() {
+        inputString("10 + 5");
+        assertThat(calculator.execute()).isEqualTo(15);
     }
 
     @Test
-    void minus() {
-
+    @DisplayName("빼기 테스트")
+    void minusTest() {
+        inputString("10 - 5");
+        assertThat(calculator.execute()).isEqualTo(5);
     }
 
     @Test
-    void multiply() {
-
+    @DisplayName("곱하기 테스")
+    void multiplyTest() {
+        inputString("10 * 5");
+        assertThat(calculator.execute()).isEqualTo(50);
     }
 
     @Test
-    void divide() {
-
+    @DisplayName("나누기 테스트")
+    void divideTest() {
+        inputString("10 / 5");
+        assertThat(calculator.execute()).isEqualTo(2);
     }
 
     @Test
-    void illegal() {
+    @DisplayName("연산 문자가 안 들어가는 경우의 예외 테스트")
+    void notOperationTest() {
+        inputString("10 $ 5");
+        assertThatThrownBy(() -> {
+            assertThat(calculator.execute());
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("연산 문자 이외에는 불가하다.");
+    }
 
+    @Test
+    @DisplayName("0으로 나누기 예외 테스트")
+    void zeroDivisionTest() {
+        inputString("10 / 0");
+        assertThatThrownBy(() -> {
+            assertThat(calculator.execute());
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("0으로 나눌 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("공백 존재 예외 테스트")
+    void blankTest() {
+        inputString("10 /  2");
+        assertThatThrownBy(() -> {
+            assertThat(calculator.execute());
+        }).isInstanceOf(NumberFormatException.class).hasMessageContaining("숫자 자리에 다른 것을 입력했거나 띄어쓰기가 잘못 되었다.");
+    }
+
+    @Test
+    @DisplayName("공백만 존재 예외 테스트")
+    void onlyBlankTest() {
+        inputString("  ");
+        assertThatThrownBy(() -> {
+            assertThat(calculator.execute());
+        }).isInstanceOf(NoSuchElementException.class).hasMessageContaining("공백은 입력 불가하다.");
+    }
+
+    @Test
+    @DisplayName("미완성 식 테스트")
+    void incompleteTest() {
+        inputString("10 / 2 *");
+        assertThatThrownBy(() -> {
+            assertThat(calculator.execute());
+        }).isInstanceOf(ArrayIndexOutOfBoundsException.class).hasMessageContaining("입력이 잘못 되었다.");
+    }
+
+    @Test
+    @DisplayName("맞지 않는 형식의 식(띄어쓰기 없는 식) 예외 테스트")
+    void noSpaceEquationTest() {
+        inputString("10/2");
+        assertThatThrownBy(() -> {
+            assertThat(calculator.execute());
+        }).isInstanceOf(NumberFormatException.class).hasMessageContaining("숫자 자리에 다른 것을 입력했거나 띄어쓰기가 잘못 되었다.");
+    }
+
+    @Test
+    @DisplayName("맞지 않는 형식의 식(연산자가 올 곳에 숫자) 예외 테스트")
+    void disorderOperationTest() {
+        inputString("10 2 2");
+        assertThatThrownBy(() -> {
+            assertThat(calculator.execute());
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("연산 문자 이외에는 불가하다.");
+    }
+
+    @Test
+    @DisplayName("맞지 않는 형식의 식(숫자가 올 곳에 연산자) 예외 테스트")
+    void disorderNumberTest() {
+        inputString("10 / *");
+        assertThatThrownBy(() -> {
+            assertThat(calculator.execute());
+        }).isInstanceOf(NumberFormatException.class).hasMessageContaining("숫자 자리에 다른 것을 입력했거나 띄어쓰기가 잘못 되었다.");
     }
 }
